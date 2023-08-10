8ab1a9311f981a19f3e30e6a4ef139cf471e4678
jradziejewski <261309@student.pwr.edu.pl>
2023-08-10 23:55:25 +0200

    feat: add page layout

---
+++ app/components/CustomButton.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { CustomButtonProps } from '../../types';
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType = 'button',
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {
+        handleClick;
+      }}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
